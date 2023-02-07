e8b4b70936f9f85e1bc5bbff0f4cae938d49677c
sayali-2001 <60540661+sayali-2001@users.noreply.github.com>
2023-02-07 09:28:53 +0530

    Use Case 3

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,12 +1,16 @@
 package WorkShop;
 
+import java.util.Scanner;
+
 public class TicTacToe {
     static  char[] board = new char[10];
+    //UC1
     public static void createBoard(){
         for(int i = 1 ; i < 10; i++){
             board[i] = ' ';
         }
     }
+    //UC1
     public static void showBoard(){
         System.out.println();
         System.out.println(" "+board[1]+" | "+board[2]+ " | "+board[3]+ " ");
@@ -15,8 +19,25 @@ public class TicTacToe {
         System.out.println("-----------------");
         System.out.println(" "+board[7]+" | "+board[8]+ " | "+board[9]+ " ");
     }
+
+    //UC2
+    public static void chooseLetter(){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter your Choice X or O");
+        char player , computer;
+        player = sc.next().charAt(0);
+        if(player == 'X'){
+            computer = 'O';
+        }
+        else {
+            computer = 'X';
+        }
+        System.out.println("You have selected : " +player);
+        System.out.println("Computers choice is : " +computer);
+    }
     public static void main(String[] args) {
         createBoard();
         showBoard();
+        chooseLetter();
     }
 }
